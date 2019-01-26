f3354ddbdbf3d7c88b2feef35099eb9b9c50f6cc
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2019-01-26 20:05:41 +0000

    Speed Fix

--- GGJ2019--Home-d98332f466b6a0df0457b78b24e900bacc1b00f7/GGJ - Home/Assets/Scripts/PlayerScript.cs
+++ GGJ2019--Home-d98332f466b6a0df0457b78b24e900bacc1b00f7/GGJ - Home/Assets/Scripts/PlayerScript.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerScript : MonoBehaviour
 {
     AudioSource hurt;
 
     [SerializeField] Animator anim;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,8 @@ public class PlayerScript : MonoBehaviour
         if (other.gameObject.tag == "small")
         {
             anim.SetBool("small" , true);
+            FirstPersonController.m_WalkSpeed = 1;
+            FirstPersonController.m_RunSpeed = 2;
         }
     }
 
