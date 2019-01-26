838fd4ab551ec47638e2b88030f206fe6181c691
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2019-01-26 15:51:24 +0000

    Script update

--- GGJ - Home/Assets/Scripts/MouseScript.cs
+++ GGJ - Home/Assets/Scripts/MouseScript.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MouseScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject Player;
+
     void Start()
     {
-        
+        GetComponent<Animator>();
+        GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            Attack();
+        }
+    }
+
+
+    public void Attack()
+    {
+        NavMeshAgent.SetDestination(Player.transform.position);
     }
 }
